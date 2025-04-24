2015ae8650bcdd94a7afaf417467f5e2c643ebcf
dnjstjs <wonseon.song@nota.ai>
2025-04-24 16:49:11 +0900

    chore: set up

---
+++ rbac/src/main/kotlin/ai/nota/rbac/RbacApplication.kt
@@ -0,0 +1,11 @@
+package ai.nota.rbac
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RbacApplication
+
+fun main(args: Array<String>) {
+	runApplication<RbacApplication>(*args)
+}
---
+++ rbac/src/test/kotlin/ai/nota/rbac/RbacApplicationTests.kt
@@ -0,0 +1,13 @@
+package ai.nota.rbac
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RbacApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
